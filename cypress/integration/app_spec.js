85051bc9ec33f5727ee2f691afefc0ec469db4f0
Dmitriy Sergeevich <dmitriy.dubotovkin@gmail.com>
2019-11-13 13:41:30 +0300

    add default cypress files

---
+++ cypress/integration/app_spec.js
@@ -0,0 +1,21 @@
+// type definitions for Cypress object "cy"
+/// <reference types="cypress" />
+
+// type definitions for custom commands like "createDefaultTodos"
+/// <reference types="../support" />
+
+// check this file using TypeScript if available
+// @ts-check
+
+// ***********************************************
+// All of these tests are written to implement
+// the official TodoMVC tests written for Selenium.
+//
+// The Cypress tests cover the exact same functionality,
+// and match the same test names as TodoMVC.
+// Please read our getting started guide
+// https://on.cypress.io/introduction-to-cypress
+//
+// You can find the original TodoMVC tests here:
+// https://github.com/tastejs/todomvc/blob/master/tests/test.js
+// ***********************************************
